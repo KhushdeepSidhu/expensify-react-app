14d6bf493923b37497f06eb32d838915ebd500d2
Khushdeep Sidhu <khushdeep009@gmail.com>
2019-06-05 09:45:31 -0400

    Setup test database environment.

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/firebase-database'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDBDha2oQVRgfaZcwww56UAEqdL2J0xADI",
-    authDomain: "expensify-9b96f.firebaseapp.com",
-    databaseURL: "https://expensify-9b96f.firebaseio.com",
-    projectId: "expensify-9b96f",
-    storageBucket: "expensify-9b96f.appspot.com",
-    messagingSenderId: "419785666491",
-    appId: "1:419785666491:web:984690a50f5dd7bc"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 }
 
 firebase.initializeApp ( firebaseConfig )
--- src/tests/setupTests.js
+++ src/tests/setupTests.js
@@ -1,6 +1,9 @@
 import Adapter from 'enzyme-adapter-react-16'
 import Enzyme from 'enzyme'
+import dotenv from 'dotenv'
 
 Enzyme.configure ( {
     adapter: new Adapter ()
 } )
+
+dotenv.config ( { path: '.env.test' } )
